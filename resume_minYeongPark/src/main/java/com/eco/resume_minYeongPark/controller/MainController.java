177691dce5f439e3aa840e3241e2f62be8c14064
MinYeong Park <judice0465@gmail.com>
2025-01-27 23:49:13 +0900

    app.tsx 파일 생성

--- resume_minYeongPark/src/main/java/com/eco/resume_minYeongPark/controller/MainController.java
+++ resume_minYeongPark/src/main/java/com/eco/resume_minYeongPark/controller/MainController.java
@@ -1,7 +1,16 @@
 package com.eco.resume_minYeongPark.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Arrays;
+import java.util.List;
 
-@Controller
+@RestController
 public class MainController {
+  @RequestMapping("/api/movie")
+  public List<String> MovieList()
+  {
+    return Arrays.asList("영화보기", "Movie");
+  }
 }
